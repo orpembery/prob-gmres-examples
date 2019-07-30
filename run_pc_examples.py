8fdbecd3c0409347b8df7dd8e22cc57477bf9be5
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-07-30 08:09:28 +0100

    PC examples with beta as 0.0,0.5,1.0.

--- run_pc_examples.py
+++ run_pc_examples.py
@@ -1,4 +1,5 @@
 from helmholtz_nearby_preconditioning.experiments import nearby_preconditioning_experiment_exponential as experiment
+import numpy as np
 
 k_range = [10.0,20.0,30.0,40.0,50.0,60.0]
 
@@ -6,10 +7,12 @@ num_repeats = 100
 
 for k in k_range:
 
-    for scale_exponent in [0.99,1.0,1.01]:
+    for scale_exponent in [0.0,0.5,1.0]:
         print(k,scale_exponent)
 
         scale = 1.0/k**scale_exponent    
 
+        np.random.set_seed(12)
+        
         experiment([k],scale,num_repeats)
 
