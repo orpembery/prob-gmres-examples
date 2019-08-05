499cae86b1b655403f13e00804c3b5d9f523f2f3
Owen Pembery <o.r.pembery@bath.ac.uk>
2019-08-05 15:31:59 +0100

    Running some more examples to get better plots.

--- run_pc_examples.py
+++ run_pc_examples.py
@@ -1,7 +1,7 @@
 from helmholtz_nearby_preconditioning.experiments import nearby_preconditioning_experiment_exponential as experiment
 import numpy as np
 
-k_range = [10.0,20.0,30.0,40.0,50.0,60.0]
+k_range = [15.0,25.0,35.0]#,40.0,50.0,60.0]
 
 num_repeats = 1000
 
